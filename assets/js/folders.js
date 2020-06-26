bea2cfb0f5f0c7c14154f489a5eec2d2df041ab9
Sava Madalin <savamadalinmihai@gmail.com>
2020-06-26 14:05:48 +0300

    working folder creation w/modal buttons.

--- assets/js/folders.js
+++ assets/js/folders.js
@@ -1,6 +1,7 @@
 window.Folders = {
     API_URL: "http://localhost:8086",
 
+
     getFolders: function () {
         $.ajax({
             url: Folders.API_URL + "/folder",
@@ -21,13 +22,51 @@ window.Folders = {
 
     getHtmlForOneFolder: function (folder) {
         return `
-        <a class="project" href="">
+        <div class="project" href="">
             <figure>
                 <img src="assets/img/folder.png" alt="">
                 <h1>${folder.name}</h1>
             </figure>
-        </a>
+        </div>
         `;
     },
-};
+
+    createFolder: function () {
+        let folderName = $("#folder_name").val();
+        let now = new Date();
+        let hour = String(now.getHours());
+        let minutes = String(now.getMinutes() + 1).padStart(2, '0');
+        let seconds = String(now.getSeconds());
+        let day = String(now.getDate()).padStart(2, '0');
+        let month = String(now.getMonth() + 1).padStart(2, '0');
+        let year = String(now.getFullYear());
+        let dateOfCreation = year + "-" + month + "-" + day + " " +
+            hour + ":" + minutes + ":" + seconds;
+
+
+        let requestBody = {
+            createdDate: dateOfCreation,
+            name: folderName,
+            numberOfFilesInside: +1
+        };
+        $.ajax({
+            url: Folders.API_URL + "/folder",
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(requestBody)
+        }).done(function () {
+            alert("Successfully created folder " + "'" + folderName + "'");
+            Folders.getFolders();
+        })
+    },
+    bindEvents: function () {
+        $(".btn-success").click(function (event) {
+            event.preventDefault();
+
+            Folders.createFolder();
+        })
+    }
+}
+
 Folders.getFolders();
+Folders.bindEvents();
